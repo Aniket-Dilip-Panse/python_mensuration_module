dd81e240f74a8d40831ecd725509d00ac9bfc757
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-10-24 15:02:22 +0530

    triangle perimeter code added

--- mensuration.py
+++ mensuration.py
@@ -6,5 +6,10 @@ class Mensuration:
         self.height = float(height)
         self.area = 0.5 * self.base * self.height
         print(f"Area of triangle with base = {self.base} and height = {self.height} is {self.area}")
+    def triangle_perimeter(self,side1,side2,side3):
+        self.side1 = float(side1)
+        self.side2 = float(side2)
+        self.side3 = float(side3)
+        self.perimeter = self.side1 + self.side2 + self.side3
+        print(f"perimeter of triangle with side1 = {self.side1}, side2 = {self.side2}, side3 = {self.side3} is {self.perimeter}")
 triangle = Mensuration()
-triangle.triangle_area(2,3)
