fa2fbf2fadc8ce210498d2559011a056320ce52e
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-10-26 23:48:22 +0530

    perimeter code  added

---
+++ perimeter.py
@@ -0,0 +1,14 @@
+def triangle_perimeter(side1,side2,side3):
+    return side1+side2+side3
+def square_perimeter(side):
+    return 4*side
+def rectangle_perimeter(length,breadth):
+    return 2 * (length+breadth)
+def parallelogram_perimeter(side1,side2,side3,side4):
+    return side1+side2+side3+side4
+def rhombus_perimeter(side):
+    return 4*side
+def trapezium_perimeter(side1,side2,side3,side4):
+    return side1+side2+side3+side4
+def circle_perimeter(radius):
+    return 2 * 3.142 * radius
