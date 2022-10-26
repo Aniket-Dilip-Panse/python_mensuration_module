8d1ec446cff26b1491ce44cd05ef919015e2bc24
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-10-26 23:40:10 +0530

    area code added

---
+++ area.py
@@ -0,0 +1,14 @@
+def triangle_area(base,height):
+    return 0.5 * base * height
+def square_area(side):
+    return side * side
+def rectangle_area(length,breadth):
+    return length * breadth
+def pallelogram_area(base,height):
+    return base * height
+def rhombus_area(diagonal1,diagonal2):
+    return 0.5 * diagonal1 * diagonal2
+def circle_area(radius):
+    3.14 * radius * radius
+def trapezium_area(side1,side2,side3,side4,height):
+    return 0.5 * (side1+side2+side3+side4) * height
