188aca953dcc2344b075c26179d43eee03342830
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-10-27 21:51:48 +0530

    perimeter code added

--- perimeter.py
+++ perimeter.py
@@ -12,3 +12,11 @@ def trapezium_perimeter(side1,side2,side3,side4):
     return side1+side2+side3+side4
 def circle_perimeter(radius):
     return 2 * 3.142 * radius
+def equilateral_triangle_perimeter(side):
+    return 3 * side
+def semicircle_perimeter(radius):
+    return (3.142 * radius) + (2 * radius)
+def right_triangle_perimeter(side1,side2,hypotenuse):
+    return side1 + side2 + hypotenuse
+def isosceles_right_triangle_perimeter(side,hypotenuse):
+    return 2*side + hypotenuse
