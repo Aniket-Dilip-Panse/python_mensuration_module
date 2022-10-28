f742aae1d56a4e3a68b92a349539b33a70bd6487
Aniket-Dilip-Panse <aniketpanse12@gmail.com>
2022-10-28 18:50:51 +0530

    description of function added

--- area.py
+++ area.py
@@ -1,14 +1,21 @@
 def triangle_area(base,height):
+    '''Enter base and height of triangle as a parameter'''
     return 0.5 * base * height
 def square_area(side):
+    '''Enter side square as a parameter'''
     return side * side
 def rectangle_area(length,breadth):
+    '''Enter length and breadth of rectangle as a parameter'''
     return length * breadth
 def pallelogram_area(base,height):
+    '''Enter base and height of parallelogram as a parameter'''
     return base * height
 def rhombus_area(diagonal1,diagonal2):
+    '''Enter diagonal1 and diagonal2 value as a parameter'''
     return 0.5 * diagonal1 * diagonal2
 def circle_area(radius):
+    '''Enter radius of circle as a parameter'''
     3.14 * radius * radius
 def trapezium_area(side1,side2,side3,side4,height):
+    '''Enter side1,side2,side3,side4,height as a parameter'''
     return 0.5 * (side1+side2+side3+side4) * height
